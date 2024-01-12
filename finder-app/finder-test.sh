e0659842689cf91058c4fb025a56c01d73eb16d4
darioguarra <dario.guarracino@wellnet.it>
2024-01-12 17:37:10 +0000

    fix finder-test.sh

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -57,7 +57,7 @@ do
 	/usr/bin/writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
 done
 
-OUTPUTSTRING=$(/usr/bin/finder.sh "$WRITEDIR" "$WRITESTR")
+OUTPUTSTRING=$(sh /usr/bin/finder.sh "$WRITEDIR" "$WRITESTR")
 
 # remove temporary directories
 rm -rf /tmp/aeld-data
